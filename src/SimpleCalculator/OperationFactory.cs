1a65e4da0fdb1b1929701adc3abf1b299660f76f
revlucio <revlucio@gmail.com>
2014-08-09 14:27:17 +0100

    refactored the operation factory

--- src/SimpleCalculator/OperationFactory.cs
+++ src/SimpleCalculator/OperationFactory.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SimpleCalculator
 {
     public class OperationFactory : IOperationFactory
     {
-        private readonly List<string> _validOperations = new List<string> { "+", "-" };
-        private readonly Dictionary<string, Func<int, int, int>> _operations = new Dictionary<string, Func<int, int, int>>();
+        private readonly Dictionary<string, Func<int, int, int>> _operations;
 
         public OperationFactory()
         {
-            _operations.Add("+", (x, y) => x + y);
-            _operations.Add("-", (x, y) => x - y);
+            _operations = new Dictionary<string, Func<int, int, int>>
+            {
+                {"+", (x, y) => x + y},
+                {"-", (x, y) => x - y}
+            };
         }
 
-        public List<string> ValidOperations { get { return _validOperations; } }
-        public Dictionary<string, Func<int, int, int>> Operations { get { return _operations; } }
+        public List<string> ValidOperations
+        {
+            get { return _operations.Keys.ToList(); }
+        }
+
+        public Dictionary<string, Func<int, int, int>> Operations
+        {
+            get { return _operations; }
+        }
     }
 }
