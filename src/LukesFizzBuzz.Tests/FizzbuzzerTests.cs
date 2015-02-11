03b4ab62d0b9b3ebbdff95a9bcd4ecd2f4b57c39
Luke Elliott <luke.elliott@blackthorn.com>
2015-02-11 18:35:06 +0000

    step 1 complete

---
+++ src/LukesFizzBuzz.Tests/FizzbuzzerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace LukesFizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzbuzzerTests
+    {
+        [TestCase(1, 2, "1 2")]
+        [TestCase(1, 4, "1 2 fizz 4")]
+        [TestCase(1, 7, "1 2 fizz 4 buzz fizz 7")]
+        [TestCase(10, 16, "buzz 11 fizz 13 14 fizzbuzz 16")]
+        public void GivenRangeOfNumbers_ThenOutputsWithSpaces(int from, int to, string expected)
+        {
+            var fizzbuzzer = new Fizzbuzzer();
+
+            var result = fizzbuzzer.Run(from, to);
+
+            result.ShouldBe(expected);
+        }
+    }
+}
---
+++ src/LukesFizzBuzz/Fizzbuzzer.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace LukesFizzBuzz
+{
+    public class Fizzbuzzer
+    {
+        public object Run(int from, int to)
+        {
+            var count = to - from + 1;
+
+            return Enumerable
+                .Range(from, count)
+                .Select(ConvertToFizzBuzz)
+                .Aggregate((x, y) => x + " " + y);
+        }
+
+        private static string ConvertToFizzBuzz(int x)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+
+            if (x % 3 == 0)
+            {
+                return "fizz";
+            }
+
+            if (x % 5 == 0)
+            {
+                return "buzz";
+            }
+
+            return x.ToString();
+        }
+    }
+}
